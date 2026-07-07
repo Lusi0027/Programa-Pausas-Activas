c00d9b33322309adebd50426da534c3013fb5bf1
Lusiany Castillo <Lusianycastillo@gmail.com>
2026-07-07 15:32:40 -0400

    agregue un boton

--- modulos/TemorizadoresForm.cs
+++ modulos/TemorizadoresForm.cs
@@ -12,5 +12,36 @@ namespace PausasActivas.Modulos
 
             // TODO Equipo 1: construir la UI aquí
         }
+
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new System.Drawing.Point(54, 44);
+            button1.Name = "button1";
+            button1.Size = new System.Drawing.Size(233, 54);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // TemorizadoresForm
+            // 
+            ClientSize = new System.Drawing.Size(532, 244);
+            Controls.Add(button1);
+            Name = "TemorizadoresForm";
+            Load += TemorizadoresForm_Load;
+            ResumeLayout(false);
+
+        }
+
+        private void TemorizadoresForm_Load(object sender, System.EventArgs e)
+        {
+
+        }
+
+        private Button button1;
     }
 }
